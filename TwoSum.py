18bc1fc16c2f16a1667d95f42823ee539f980b19
yqu212 <yqu212@gmail.com>
2015-04-05 19:42:28 +0800

    Add the solution to Two Sum. Pass.

--- TwoSum.py
+++ TwoSum.py
@@ -1,42 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# class Solution:
-#     # @return a tuple, (index1, index2)
-#     def twoSum(self, num, target):
-#         diff = [target - i for i in num]
-#         TwoSum = [[num.index(i)+1, diff.index(i)+1] for i in diff if i in num]
-#         t = TwoSum[0]
-#         t.sort()
-#         return tuple(t)
-
-
-                
-        # diff = [target - i for i in num]
-        # TwoSum = [[num.index(i)+1, diff.index(i)+1] for i in diff if i in num]
-        # t = TwoSum[0]
-        # t.sort()
-        # return tuple(t)
-
 class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
-        for i in num:
-            diff = target - i
-            if diff == i:
-                index1 = num.index(i)
-                num.remove(i)
-                if diff in num:
-                    return (index1+1, num.index(diff)+2)
-            else:
-                if diff in num:
-                # print num.index(diff)
-                # print num.index(i)
-                    if num.index(diff) < num.index(i):
-                        return (num.index(diff)+1, num.index(i)+1)
-                    elif num.index(diff) > num.index(i):
-                        return (num.index(i)+1, num.index(diff)+1)
-        
-    
+        d = {}
+        for index in xrange(len(num)):
+            diff = target - num[index]
+            if diff in d:
+                return (d[diff] + 1, index + 1)
+            d[num[index]] = index
+
 s = Solution()
 print s.twoSum([0,4,3,0], 0)
