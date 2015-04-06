4d7add470ffcd2b98dd1054ef6e431d3241987fb
yqu212 <yqu212@gmail.com>
2015-04-06 10:37:01 +0800

    Add the solution to Length of Last Word. Fail.

--- LengthofLastWord.py
+++ LengthofLastWord.py
@@ -3,15 +3,19 @@ class Solution:
     # @return an integer
     def lengthOfLastWord(self, s):
         count = 0
-
         if len(s) == 0:
             return count
 
-        for i in s[::-1]:
-            if i==' ':
+        i = -1
+        while True:
+            print i
+            if s[i]==' ':
                 return count
             else:
                 count = count + 1
+                if -i == len(s):
+                    return count
+                i = i - 1
 
 s = Solution()
-print s.lengthOfLastWord('a')
+print s.lengthOfLastWord('a ')
